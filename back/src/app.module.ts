0db5756a5288486698c5a3ca430c0ef45953ae7c
x2makeline <x2makeline@gmail.com>
2023-08-17 01:13:40 +0900

    websocket test

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+      exclude: ['/api/(.*)'],
+    }),
+    EventsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
